317828ccd58644ebf6b47c9eaa94c0b24afa16db
thebmac <78640403+Thebai2357@users.noreply.github.com>
2024-10-10 01:10:51 +0900

    add start time, end time, place

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,7 +1,24 @@
+import 'package:flutter/material.dart';
+
 class Event{
   final String title;
-  Event(this.title);
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+  final String location;
+  Event({
+    required this.title,
+    required this.startTime,
+    required this.endTime,
+    required this.location,
+  });
+  String _formatTime(TimeOfDay time){
+    final hour = time.hour.toString().padLeft(2,'0');
+    final minute = time.minute.toString().padLeft(2,'0');
+    return '$hour:$minute';
+  }
 
   @override
-  String toString() => title;
+  String toString(){
+    return '$title (${_formatTime(startTime)} - ${_formatTime(endTime)}) @ $location';
+  }
 }
--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -56,8 +56,10 @@ class _CalendarPageState extends State<CalendarPage>{
               children: _getEventsForDay(_selectedDay)
               .map((event)=> ListTile(
                 title: Text(event.title),
-              ))
-              .toList(),
+                subtitle: Text(
+                  '${event.startTime.format(context)} - ${event.endTime.format(context)} @ ${event.location}'
+                ),
+              )).toList(),
               ),
           ),
         ],
@@ -78,40 +80,153 @@ class _CalendarPageState extends State<CalendarPage>{
   }
 
   void _addEvent(){
-    String eventTitle = '';
+    final _titleController = TextEditingController();
+    final _locationController = TextEditingController();
+    TimeOfDay? _startTime;
+    TimeOfDay? _endTime;
+
     showDialog(
       context: context, 
-      builder: (context) => AlertDialog(
-        title: const Text('予定を追加'),
-        content: TextField(
-          autofocus: true,
-          decoration: const InputDecoration(hintText:'予定を入力'),
-          onChanged:  (value){
-            eventTitle = value;
-          },
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-             child: const Text('cancel')
-          ),
-          TextButton(
-            onPressed: (){
-              if (eventTitle.isEmpty) return;
-              setState(() {
-                DateTime selectedDayOnly = _getDataOnly(_selectedDay);
-                if (_events[selectedDayOnly]!= null){
-                  _events[selectedDayOnly]!.add(Event(eventTitle));
-                } else {
-                  _events[selectedDayOnly] = [Event(eventTitle)];
-                }
-              });
-              Navigator.pop(context);
-            },
-            child: const Text('追加'),
-          ),
-        ],
+      builder: (context) => StatefulBuilder(
+        builder: (context,setState){
+          return AlertDialog(
+            title: const Text('予定を追加'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(hintText: 'タイトルを入力'),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () async{
+                        _startTime = await showTimePicker(
+                          context: context, 
+                          initialTime: TimeOfDay.now(),
+                        );
+                        if (_startTime != null){
+                          setState(() {});
+                        }
+                      }, 
+                      child: Text(
+                        _startTime == null
+                          ? '開始時間を選択'
+                          : '開始: ${_startTime!.format(context)}',
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () async{
+                        _endTime = await showTimePicker(
+                          context: context, 
+                          initialTime: TimeOfDay.now(),
+                        );
+                        if (_endTime != null){
+                          setState(() {});
+                        }
+                      }, 
+                      child: Text(
+                        _endTime == null
+                          ? '終了時間を選択'
+                          : '終了: ${_startTime!.format(context)}',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _locationController,
+                  decoration: const InputDecoration(hintText: '場所を入力'),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed:(){
+                  if (_titleController.text.isEmpty||
+                  _startTime == null||
+                  _endTime == null||
+                  _locationController.text.isEmpty){
+                    return;
+                  }
+                  setState((){
+                    DateTime selectedDayOnly = _getDataOnly(_selectedDay);
+                    if (_events[selectedDayOnly] != null){
+                      _events[selectedDayOnly]!.add(
+                        Event(
+                          title: _titleController.text, 
+                          startTime: _startTime!, 
+                          endTime: _endTime!, 
+                          location: _locationController.text,
+                        ),
+                      );
+                    }else{
+                      _events[selectedDayOnly] = [
+                        Event(
+                          title: _titleController.text,
+                          startTime: _startTime!,
+                          endTime: _endTime!,
+                          location: _locationController.text,
+                        ),
+                      ];
+                    }
+                    });
+                    Navigator.pop(context);
+                  },
+                  child: const Text('追加'),  
+                ),
+            ],
+          );
+        },
       ),
     );
-  } 
+  }
+
+  // void _addEvent(){
+  //   String eventTitle = '';
+  //   showDialog(
+  //     context: context, 
+  //     builder: (context) => AlertDialog(
+  //       title: const Text('予定を追加'),
+  //       content: TextField(
+  //         autofocus: true,
+  //         decoration: const InputDecoration(hintText:'予定を入力'),
+  //         onChanged:  (value){
+  //           eventTitle = value;
+  //         },
+  //       ),
+  //       actions: [
+  //         TextButton(
+  //           onPressed: () => Navigator.pop(context),
+  //            child: const Text('cancel')
+  //         ),
+  //         TextButton(
+  //           onPressed: (){
+  //             if (eventTitle.isEmpty) return;
+  //             setState(() {
+  //               DateTime selectedDayOnly = _getDataOnly(_selectedDay);
+  //               if (_events[selectedDayOnly]!= null){
+  //                 _events[selectedDayOnly]!.add(Event(eventTitle));
+  //               } else {
+  //                 _events[selectedDayOnly] = [Event(eventTitle)];
+  //               }
+  //             });
+  //             Navigator.pop(context);
+  //           },
+  //           child: const Text('追加'),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // } 
 }
